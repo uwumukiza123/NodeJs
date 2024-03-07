5a8f52650e49a0331125974e918469e95dcbf140
uwumukiza123 <uwumukiza123>
2024-03-08 01:40:04 +0200

    introducing lodash

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http')
 const fs = require('fs')
+const _ = require('lodash')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
+    // lodash
+    const num = _.random(0, 20)
+    console.log(num)
+
+    const greet = _.once(() => {
+        console.log('hello')
+    })
+    greet()
+    greet()
 
     // set header content-type
     res.setHeader('Content-type', 'text/html');
@@ -17,6 +26,12 @@ const server = http.createServer((req, res) => {
         case '/about':
             path += 'about.html';
             res.statusCode = 200;
+            break;
+
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('location', '/about')
+            res.end()
             break;
 
         default:

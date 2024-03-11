e18530333b1a8f2f6986dbe078a67dbd781af680
uwumukiza123 <uwumukiza123>
2024-03-11 11:03:48 +0200

    connect to the database

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+const Blog = require('./models/blog');
+
 // express app
 const app = express();
+
+// connect to mongodb
+
+const dbURI = 'mongodb+srv://netNinja:netNinja123@cluster0.nlx1am2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.connect(dbURI)
+.then((result) => app.listen(3000)
+)
+.catch((err) => console.log(err))
+
 
 // Register view engine
 app.set('view engine', 'ejs')
 
-// listen for requests
-app.listen(3000);
+// mongoose and mongo sandbox
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    }); 
+
+    blog.save()
+        .then((result) => {
+            res.send(result)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+})
+
+// middleware & static files
+app.use(express.static('public'))
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     // res.send('<p>Home page</p>')
